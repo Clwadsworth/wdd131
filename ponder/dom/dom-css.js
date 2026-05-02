317f275a0eaa3e19cd553699bc47659b18406d7c
Clwadsworth <clwadsworth93@byui.edu>
2026-05-02 09:08:12 -0600

    Continual working

---
+++ ponder/dom/dom-css.js
@@ -0,0 +1,14 @@
+const title = document.querySelector('h1');
+
+console.log(title)
+
+title.textContent = 'Web Page Components';
+
+// Different way of stating the same thing aa the all in one.
+
+// let topics = document.querySelector('#topics');
+// topics.style.color = 'blue';
+
+document.querySelector('#topics').style.color = 'red';
+
+// document.getElementById('topics').style.color = 'purple';
