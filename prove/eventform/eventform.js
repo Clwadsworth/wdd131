d894a940bb74c592bca4bec4d6083a4d670127d0
Clwadsworth <clwadsworth93@byui.edu>
2026-06-06 22:45:42 -0600

    Finalized JS for Eventform

--- prove/eventform/eventform.js
+++ prove/eventform/eventform.js
@@ -10,10 +10,14 @@ function updateIdField() {
 
     if (value === 'student') {
         studentContainer.hidden = false;
-        idStd.required = true
+        guestContainer.hidden = true;
+        idStd.required = true;
+        idGst.required = false;
     } else if (value === 'guest') {
         guestContainer.hidden = false;
+        studentContainer.hidden = true;
         idGst.required = true;
+        idStd.required = false;
     } else {
         studentContainer.hidden = true;
         idStd.required = false;
@@ -23,11 +27,13 @@ function updateIdField() {
 }
 
 idForm.addEventListener("change", updateIdField);
-updateIdField;
+updateIdField();
 
 function isPastDate(value) {
     const today = new Date();
+    today.setHours(0, 0, 0, 0);
     const chosen = new Date(value);
+    chosen.setHours(0, 0, 0, 0);
     return chosen < today;
 }
 
@@ -39,28 +45,44 @@ form.addEventListener("submit", function (event) {
     const lastName = form.lastName.value.trim();
     const email = form.email.value.trim();
     const idInform = form.idForm.value.trim();
-    const availableDate = form.availableDate.value;
-    // const for student and guest?
+    const eventDate = form.eventDate.value;
+    const studentId = idStd.value.trim();
+    const guestCode = idGst.value.trim();
 
-        if () {
+        if (idInform === "") {
+            output.textContent = "Please choose if you are a student or a guest.";
             return;
         }
         
-        if () {
+        if (idInform === "student" && studentId.length < 9) {
+            output.textContent = "Your Student ID must be 9 digits.";
             return;
         }
 
-        if () {
+        if (idInform === "guest" && guestCode !== "EVENT131") {
+            output.textContent = "Access code is incorrect. Please enter a valid access code."
             return;
         }
 
-    if (isPastDate(availableDate)) {
+    if (isPastDate(eventDate)) {
         output.textContent = "Please choose a later date.";
         return;
     }
 
+    let ticketIdLine = "";
+    if (idInform === "student") {
+        ticketIdLine = `<p>Student ID: ${studentId}</p>`
+    } else {
+        ticketIdLine = `<p>Access Code: ${guestCode}</p>`
+    }
+
     output.innerHTML = `
-    
+        <h2>Ticket Created!<h2>
+        <p>${firstName} ${lastName}</p>
+        <p>Email: ${email}</p>
+        <p>Confirmed: ${eventDate}</p>
+        <p>Type: ${idInform}</p>
+        ${ticketIdLine}
     `;
 
     form.reset();
